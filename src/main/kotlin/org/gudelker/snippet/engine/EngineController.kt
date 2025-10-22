26bb92dcdc7d09969a52e08ab2f13d13ab909d1c
Agustin <130694104+Agustinguglielmelli@users.noreply.github.com>
2025-10-22 20:51:21 -0300

    started parse endpoint, missing StreamingPipeline logic (#2)

---
+++ src/main/kotlin/org/gudelker/snippet/engine/EngineController.kt
@@ -0,0 +1,18 @@
+package org.gudelker.snippet.engine
+
+import org.gudelker.utilities.Version
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/snippet")
+class EngineController (private val service: EngineService) {
+
+    @PostMapping("/parse")
+    fun parseSnippet(snippetContent: String, version: Version): ResultType {
+        val parser = service.createParser(version)
+        val lexer = service.createLexer(version)
+        return service.parseSnippet(lexer,parser, snippetContent)
+    }
+}
