309fc6cbf5560eff6449c7fa7ac2d7d1087dea45
pedrodelaguila <pdelaguila@mail.austral.edu.ar>
2025-11-18 14:40:05 -0300

    feat: redis config

--- src/main/kotlin/org/gudelker/snippet/engine/redis/RedisStreamConfig.kt
+++ src/main/kotlin/org/gudelker/snippet/engine/redis/RedisStreamConfig.kt
@@ -2,18 +2,15 @@ package org.gudelker.snippet.engine.redis
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import org.gudelker.snippet.engine.utils.dto.LintRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.connection.stream.ObjectRecord
+import org.springframework.data.redis.connection.stream.MapRecord
 import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.StringRedisSerializer
 import org.springframework.data.redis.stream.StreamMessageListenerContainer
 import java.time.Duration
-import kotlin.jvm.java
 
 @Configuration
 @Profile("!test")
@@ -24,28 +21,25 @@ class RedisStreamConfig(
     fun objectMapper(): ObjectMapper = ObjectMapper().registerKotlinModule()
 
     @Bean
-    fun streamListenerContainer(): StreamMessageListenerContainer<String, ObjectRecord<String, LintRequest>> {
+    fun streamListenerContainer(): StreamMessageListenerContainer<String, MapRecord<String, String, String>> {
         val options =
             StreamMessageListenerContainer.StreamMessageListenerContainerOptions
                 .builder()
                 .pollTimeout(Duration.ofSeconds(1))
-                .targetType(LintRequest::class.java) // convierte JSON -> LintRequest
+                // No especificar targetType para recibir como MapRecord
                 .build()
 
         return StreamMessageListenerContainer.create(factory, options)
     }
 
     @Bean
-    fun redisTemplate(
-        factory: RedisConnectionFactory,
-        objectMapper: ObjectMapper,
-    ): RedisTemplate<String, LintRequest> {
-        val template = RedisTemplate<String, LintRequest>()
+    fun redisTemplate(factory: RedisConnectionFactory): RedisTemplate<String, String> {
+        val template = RedisTemplate<String, String>()
         template.connectionFactory = factory
         template.keySerializer = StringRedisSerializer()
         template.hashKeySerializer = StringRedisSerializer()
-        template.valueSerializer = GenericJackson2JsonRedisSerializer(objectMapper)
-        template.hashValueSerializer = GenericJackson2JsonRedisSerializer(objectMapper)
+        template.valueSerializer = StringRedisSerializer()
+        template.hashValueSerializer = StringRedisSerializer()
         template.afterPropertiesSet()
         return template
     }
