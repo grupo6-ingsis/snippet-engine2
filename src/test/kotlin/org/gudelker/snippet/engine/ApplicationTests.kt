696b024a1899b192fb5bbb911e1b817429851617
Agustín Guglielmelli <agusguglielmelli@gmail.com>
2025-11-18 10:32:21 -0300

    mock to initialize app with redis mock

--- src/test/kotlin/org/gudelker/snippet/engine/ApplicationTests.kt
+++ src/test/kotlin/org/gudelker/snippet/engine/ApplicationTests.kt
@@ -1,16 +1,25 @@
 package org.gudelker.snippet.engine
 
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.mock
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Primary
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.test.context.ActiveProfiles
 
 @ActiveProfiles("test")
 @SpringBootTest
 class ApplicationTests {
-    @MockBean
-    private lateinit var redisConnectionFactory: RedisConnectionFactory
+    @TestConfiguration
+    class TestRedisConfig {
+        @Bean
+        @Primary
+        fun redisConnectionFactory(): RedisConnectionFactory {
+            return mock(RedisConnectionFactory::class.java)
+        }
+    }
 
     @Test
     fun contextLoads() {
